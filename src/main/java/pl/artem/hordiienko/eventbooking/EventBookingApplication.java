377e1850f12b1e84b2cca30925da41a5aca3c8ba
arte-code <268686271+arte-code@users.noreply.github.com>
2026-04-20 11:22:38 +0200

    initialize Spring Boot application

---
+++ src/main/java/pl/artem/hordiienko/eventbooking/EventBookingApplication.java
@@ -0,0 +1,14 @@
+package pl.artem.hordiienko.eventbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EventBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/artem/hordiienko/eventbooking/EventBookingApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.artem.hordiienko.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class EventBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
